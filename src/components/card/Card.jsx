205fcb980bed3c828e9a9df7b89ce7b0eae883ff
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-25 18:47:44 -0300

    add: useEffect, useState and use fecth to get data form the API to display on the header and h1 tags inside of the card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const RANDOM_ADVICE = 'https://api.adviceslip.com/advice';
+
 export default function Card() {
+  const [advice, setAdvice] = useState();
+  const [id, setId] = useState();
+
+  useEffect(() => {
+    fetch(RANDOM_ADVICE)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const { ...advice } = data.slip;
+        setId(advice.id);
+        setAdvice(advice.advice);
+      });
+  }, []);
+
   return (
     <section className=' flex flex-col items-center w-full h-full p-8 justify-center'>
-      <article className='bg-neutralDarkGrayish w-full h-3/5 p-8'>
+      <article className='bg-neutralDarkGrayish w-full h-3/5 p-8 rounded-xl'>
         <div className='flex flex-col items-center'>
           <header>
-            <span className='text-primaryNeonGreen'>Advice #117</span>
+            <span className='text-primaryNeonGreen'>Advice #{id}</span>
           </header>
-          <h1 className='text-primaryLightCyan'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <h1 className='text-primaryLightCyan font-xl'>
+            &quot;{advice}&quot;
           </h1>
           <footer></footer>
           <img src='' alt='' />
