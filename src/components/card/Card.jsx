a2aba04914a4fb6753bff61de45e21e3bd72ef4c
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-25 18:13:31 -0300

    add: styles to the elements of the card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,11 +2,13 @@ export default function Card() {
   return (
     <section className=' flex flex-col items-center w-full h-full p-8 justify-center'>
       <article className='bg-neutralDarkGrayish w-full h-3/5 p-8'>
-        <div>
+        <div className='flex flex-col items-center'>
           <header>
-            <span>Advice #117</span>
+            <span className='text-primaryNeonGreen'>Advice #117</span>
           </header>
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1 className='text-primaryLightCyan'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
           <footer></footer>
           <img src='' alt='' />
         </div>
