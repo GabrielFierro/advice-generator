b8bcb278b61ad347c018b54ad860ce58e707bd48
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-25 18:02:59 -0300

    add: tags and classes to give it structure of a card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,3 +1,16 @@
 export default function Card() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <section className=' flex flex-col items-center w-full h-full p-8 justify-center'>
+      <article className='bg-neutralDarkGrayish w-full h-3/5 p-8'>
+        <div>
+          <header>
+            <span>Advice #117</span>
+          </header>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <footer></footer>
+          <img src='' alt='' />
+        </div>
+      </article>
+    </section>
+  );
 }
