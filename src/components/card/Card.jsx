59abfd0ac09a58cd5db7e5d7d986be4d89bd547e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-26 18:50:06 -0300

    add: classes to style the text and images in the footer on the card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import PatternDivider from '../../../images/pattern-divider-mobile.svg';
 
 const RANDOM_ADVICE = 'https://api.adviceslip.com/advice';
 
@@ -20,16 +21,36 @@ export default function Card() {
 
   return (
     <section className=' flex flex-col items-center w-full h-full p-8 justify-center'>
-      <article className='bg-neutralDarkGrayish w-full h-3/5 p-8 rounded-xl'>
-        <div className='flex flex-col items-center'>
-          <header>
-            <span className='text-primaryNeonGreen'>Advice #{id}</span>
+      <article className='bg-neutralDarkGrayish w-full h-auto p-8 rounded-xl'>
+        <div className='flex flex-col items-center font-manrope-extrabold font-extrabold text-center'>
+          <header className=' tracking-widest'>
+            <span className='text-primaryNeonGreen uppercase tracking-widest text-md'>
+              Advice #{id}
+            </span>
           </header>
-          <h1 className='text-primaryLightCyan font-xl'>
+          <h1 className='text-primaryLightCyan text-3xl py-8'>
             &quot;{advice}&quot;
           </h1>
-          <footer></footer>
-          <img src='' alt='' />
+          <footer className='flex flex-col items-center justify-center'>
+            <div className='pb-6'>
+              <img src={PatternDivider} alt='Pattern divider' />
+            </div>
+            <div className='w-16 h-16 rounded-full bg-primaryNeonGreen flex items-center justify-center -mb-16'>
+              <div className='w-auto h-auto'>
+                <svg
+                  className='bg-primaryNeonGreen w-full h-full cursor-pointer'
+                  width='24'
+                  height='24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z'
+                    fill='#202733'
+                  />
+                </svg>
+              </div>
+            </div>
+          </footer>
         </div>
       </article>
     </section>
